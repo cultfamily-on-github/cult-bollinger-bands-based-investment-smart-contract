0b1d42c7e6901fd8fc09557ef273c26d98b846ae
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2022-10-03 17:01:28 +0200

    Create bollinger-bands.sol

---
+++ bollinger-bands.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+// see https://deno.land/x/bollinger_bands it was simple for me to implement this in typescript
+// it seems challenging to me to implement this in solidity
+
+contract BollingerBands {
+
+
+    function getBollingerBands() {
+        // see https://github.com/VoFarm/bollinger_bands/blob/main/src/bollinger-bands-service.ts#L11
+    }
+
+    function substractMeanFromEachAndSquare() {
+        // see https://github.com/VoFarm/bollinger_bands/blob/main/src/bollinger-bands-service.ts#L39
+    }
+
+    function getStandardDeviation() {
+        // see https://github.com/VoFarm/bollinger_bands/blob/main/src/bollinger-bands-service.ts#L56
+    }
+
+    function calculateAverage() {
+        // see https://github.com/VoFarm/bollinger_bands/blob/main/src/bollinger-bands-service.ts#L69
+    }
+
+    function substractMeanFromEachAndSquare() {
+        // see https://github.com/VoFarm/bollinger_bands/blob/main/src/bollinger-bands-service.ts#L81
+    }
+
+}
