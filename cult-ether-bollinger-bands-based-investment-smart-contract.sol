82136642a1c339ba7cf970a9f824eac0f9e40b17
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2022-10-03 17:08:00 +0200

    Create cult-ether-bollinger-bands-based-investment-smart-contract.sol

---
+++ cult-ether-bollinger-bands-based-investment-smart-contract.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract CultEtherBollingerBandsBasedInvestmentSmartContract {
+
+    // will long-term invest in Ether and CULT based on long- mid- and short- term Bollinger Bands
+    
+    // https://github.com/cultfamily-on-github/cult-bollinger-bands-based-investment-smart-contract/blob/main/bollinger-bands.sol
+
+}
